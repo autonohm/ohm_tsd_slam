18d14bfc599ecbec16b7f13e09d4adf2d72f47dc
Philipp Koch <philipp.koch@th-nuernberg.de>
2014-11-13 17:30:40 +0100

    Cleaning up. Removed unnecessary variables and cleaned comments.

--- src/multi/ThreadLocalize.cpp
+++ src/multi/ThreadLocalize.cpp
@@ -17,7 +17,6 @@
 #include <boost/bind.hpp>
 
 #include <cstring>
-#include <string>
 #include <unistd.h>
 
 #include <geometry_msgs/PoseStamped.h>
@@ -28,71 +27,33 @@ namespace ohm_tsd_slam
 
 ThreadLocalize::ThreadLocalize(obvious::TsdGrid* grid, ThreadMapping* mapper, boost::mutex* pubMutex, std::string nameSpace,
     const double xOffFactor, const double yOffFactor):
-            _sensor(NULL),
-            _newScan(false),
-            _initialized(false),
-            _mapper(*mapper),
-            _grid(*grid)
+        _grid(*grid),
+        _mapper(*mapper),
+        _nameSpace(nameSpace),
+        _localizer(NULL),
+        _sensor(NULL),
+        _newScan(false),
+        _initialized(false)
 {
-  /**
-   * width
-   * height
-   * namespace
-   * laser topic
-   * x offset
-   * y offset
-   * yaw offset
-   * tf_base
-   * tf_child
-   */
-
   ros::NodeHandle prvNh("~");
 
   std::string laserTopic;
   prvNh.param("laser_topic", laserTopic, std::string("scan"));
   laserTopic = nameSpace + "/" + laserTopic;
-
-  std::string xOffParamServer;
-  xOffParamServer = nameSpace + "/x_offset";
-  prvNh.param<double>(xOffParamServer, _xOffset, 0.0);
-
-  std::string yOffParamServer;
-  yOffParamServer = nameSpace + "/y_offset";
-  prvNh.param<double>(yOffParamServer, _yOffset, 0.0);
-
-  std::string yawOffParamServer;
-  yOffParamServer = nameSpace + "/yaw_offset";
-  prvNh.param<double>(yOffParamServer, _yawOffset, 0.0);
-
-  std::string maxRangeParamServer;
-  maxRangeParamServer = nameSpace + "/max_range";
-  prvNh.param<double>(maxRangeParamServer, _maxRange, 30.0);
-
-  std::string minRangeParamServer;
-  maxRangeParamServer = nameSpace + "/min_range";
-  prvNh.param<double>(maxRangeParamServer, _minRange, 0.001);
-
-  std::string footPrintWidthParamServer;
-  footPrintWidthParamServer = nameSpace + "/footprint_width";
-  prvNh.param<double>(footPrintWidthParamServer, _footPrintWidth, 0.0);
-
-
-  std::string footPrintHeightParamServer;
-  footPrintHeightParamServer = nameSpace + "/footprint_height";
-  prvNh.param<double>(footPrintHeightParamServer, _footPrintHeight, 0.0);
-  double _footPrintHeight;
-
-  _gridWidth = grid->getCellsX() * grid->getCellSize();
+  prvNh.param<double>(nameSpace + "/x_offset"        , _xOffset, 0.0);
+  prvNh.param<double>(nameSpace + "/y_offset"        , _yOffset, 0.0);
+  prvNh.param<double>(nameSpace + "/yaw_offset"      , _yawOffset, 0.0);
+  prvNh.param<double>(nameSpace + "/max_range"       , _maxRange, 30.0);
+  prvNh.param<double>(nameSpace + "/min_range"       , _minRange, 0.001);
+  prvNh.param<double>(nameSpace + "/footprint_width" , _footPrintWidth, 0.0);
+  prvNh.param<double>(nameSpace + "/footprint_height", _footPrintHeight, 0.0);
+  std::cout << __PRETTY_FUNCTION__ << " min range set but currently not used." << std::endl;
+  _gridWidth  = grid->getCellsX() * grid->getCellSize();
   _gridHeight = grid->getCellsY() * grid->getCellSize();
-  double _gridHeight;
-
   _xOffFactor = xOffFactor;
   _yOffFactor = yOffFactor;
-
-  _localizer = new Localization(grid, mapper, pubMutex, _xOffFactor, _yOffFactor, nameSpace);
-  _sensor = NULL;
-
-  _lasSubs = _nh.subscribe(laserTopic, 1, &ThreadLocalize::laserCallBack, this);
+  _localizer  = new Localization(grid, mapper, pubMutex, _xOffFactor, _yOffFactor, nameSpace);
+  _lasSubs    = _nh.subscribe(laserTopic, 1, &ThreadLocalize::laserCallBack, this);
 }
 
 ThreadLocalize::~ThreadLocalize()
@@ -113,7 +74,7 @@ void ThreadLocalize::eventLoop(void)
     }
     else
     {
-      //maybe publish pose only ?
+      //toDo: maybe publish pose only ?
     }
     _newScan = false;
   }
@@ -131,17 +92,13 @@ void ThreadLocalize::init(const sensor_msgs::LaserScan& scan)
       std::sin(phi),  std::cos(phi), startY,
       0,              0,      1};
 
-
   obvious::Matrix Tinit(3, 3);
   Tinit.setData(tf);
-  std::cout << __PRETTY_FUNCTION__ << "Tinit = \n" << Tinit << std::endl;
   _sensor->transform(&Tinit);
   obfloat t[2] = {startX, startY};
   if(!_grid.freeFootprint(t, _footPrintWidth, _footPrintHeight))
     std::cout << __PRETTY_FUNCTION__ << " warning! Footprint could not be freed!\n";
   _mapper.initPush(_sensor);
-
-
   _initialized = true;
 
 }
@@ -154,15 +111,7 @@ void ThreadLocalize::laserCallBack(const sensor_msgs::LaserScan& scan)
     this->init(scan);
     std::cout << __PRETTY_FUNCTION__ << " initialized -> running...\n";
   }
-  //  for(unsigned int i=0;i<scan.ranges.size();i++)
-  //  {
-  //
-  //    _mask[i] = !isnan(scan.ranges[i]) && !isinf(scan.ranges[i]) && (fabs(scan.ranges[i]) > 10e-6);
-  //    if((_rangeFilter)&&_mask[i])
-  //      _mask[i]=(scan.ranges[i]>_minRange)&&(scan.ranges[i]<_maxRange);
-  //  }
   _sensor->setRealMeasurementData(scan.ranges, 1.0);
-  //_sensor->setRealMeasurementMask(_mask);
   _newScan = true;
 }
 
--- src/multi/ThreadLocalize.h
+++ src/multi/ThreadLocalize.h
@@ -18,12 +18,7 @@
 #include "obvision/reconstruct/grid/SensorPolar2D.h"
 #include "obvision/reconstruct/grid/TsdGrid.h"
 
-//#define ITERATIONS 25
-//#define TRNS_THRESH 1.5            //Thresholds for registration. If the gained transformation is out of these bounds,
-//#define ROT_THRESH 0.6             //the Transformation is not taken over
-//#define TRNS_MIN 0.05              //Minimal values for the pose change. Push is only needed when pose change
-//#define ROT_MIN 0.09               //greater than than one of these values
-
+#include <string>
 
 namespace ohm_tsd_slam
 {
@@ -36,36 +31,34 @@ class ThreadLocalize: public ThreadSLAM
 {
 public:
   ThreadLocalize(obvious::TsdGrid* grid, ThreadMapping* mapper, boost::mutex* pubMutex, std::string nameSpace,
-                 const double xOffFactor, const double yOffFactor);
+      const double xOffFactor, const double yOffFactor);
   virtual ~ThreadLocalize();
 protected:
   virtual void eventLoop(void);
 private:
   void laserCallBack(const sensor_msgs::LaserScan& scan);
   void init(const sensor_msgs::LaserScan& scan);
+
   ros::NodeHandle _nh;
   ros::Subscriber _lasSubs;
-
-  obvious::SensorPolar2D* _sensor;
   obvious::TsdGrid& _grid;
-  bool _newScan;
-  Localization* _localizer;
   ThreadMapping& _mapper;
-  double _maxRange;
-  double _minRange;
-  double _yawOffset;
+  std::string _nameSpace;
+  obvious::SensorPolar2D* _sensor;
+  Localization* _localizer;
+  bool _newScan;
+  bool _initialized;
   double _xOffset;
   double _yOffset;
+  double _yawOffset;
+  double _maxRange;
+  double _minRange;
+  double _footPrintWidth;
+  double _footPrintHeight;
   double _gridWidth;
   double _gridHeight;
   double _xOffFactor;
   double _yOffFactor;
-  bool _initialized;
-  double _footPrintWidth;
-  double _footPrintHeight;
-  //MultiSlamNode& _parentNode;
-
-
 };
 
 } /* namespace ohm_tsd_slam */

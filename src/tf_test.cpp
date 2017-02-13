b1913de283cb0a0545853486adca9c93f9e3a2be
Your Name <you@example.com>
2017-02-13 23:10:57 +0100

    Updadet test program.

--- src/tf_test.cpp
+++ src/tf_test.cpp
@@ -32,37 +32,62 @@ void callbackTimer(const ros::TimerEvent&)
   ohm_tsd_slam::UtilitiesTransform utils;
   obvious::Matrix mat1(3, 3);
   obvious::Matrix mat2(3, 3);
-  mat1.setIdentity();
-  mat1(1, 2) = 1.0;
-  mat2.setIdentity();
-  mat2(1, 2) = 2.0;
-  tf::Transform tf;
-  tf.setRotation(tf::createQuaternionFromYaw(0.8));
-  tf.setOrigin(tf::Vector3(1.0, 0.0, 0.0));
-  mat1 = utils.tfToObviouslyMatrix3x3(tf);
-  bc.sendTransform(tf::StampedTransform(tf, ros::Time::now(), "map", "tf1"));
-  obvious::Matrix point(1, 3);
-  point.setIdentity();
-  point(0, 0) = 1.0;
-  point = mat1 * point;
-  geometry_msgs::PoseStamped
+  obvious::Matrix mat3(3, 3);
+  obvious::Matrix matsum(3, 3);
+  tf::Transform tf1;
+  tf::Transform tf2;
+  tf::Transform tf3;
+  tf::Transform tfsum;
 
-  tf.setRotation(tf::createQuaternionFromYaw(1.2));
-  tf.setOrigin(tf::Vector3(2.0, 0.0, 0.0));
-  mat2 = utils.tfToObviouslyMatrix3x3(tf);
-  bc.sendTransform(tf::StampedTransform(tf, ros::Time::now(), "map", "tf2"));
+  tf1.setOrigin(tf::Vector3(0.0, 1.0, 0.0));
+  tf1.setRotation(tf::createQuaternionFromYaw(0.0));
+  bc.sendTransform(tf::StampedTransform(tf1, ros::Time::now(), "map", "tf1"));
+  tf2.setOrigin(tf::Vector3(0.0, 1.0, 0.0));
+  mat1 = utils.tfToObviouslyMatrix3x3(tf1);
+  tf2.setRotation(tf::createQuaternionFromYaw(0.8));
+  bc.sendTransform(tf::StampedTransform(tf2, ros::Time::now(), "tf1", "tf2"));
+  tf3.setOrigin(tf::Vector3(0.0, 1.0, 0.0));
+  mat2 = utils.tfToObviouslyMatrix3x3(tf2);
+  tf3.setRotation(tf::createQuaternionFromYaw(0.8));
+  bc.sendTransform(tf::StampedTransform(tf3, ros::Time::now(), "tf2", "tf3"));
+  mat3 = utils.tfToObviouslyMatrix3x3(tf3);
+  matsum = mat1 * mat2 * mat3;
+  //matsum = matsum ;
+  tfsum = utils.obviouslyMatrix3x3ToTf(matsum);
+  //bc.sendTransform(tf::StampedTransform(tfsum, ros::Time::now(), "map", "tf3"));
 
 
-  obvious::Matrix matDiff(3, 3);
-  matDiff.setIdentity();
-  matDiff = mat2 * mat1.getInverse();
-  matDiff.print();
-  tf = utils.obviouslyMatrix3x3ToTf(matDiff);
-  //bc.sendTransform(tf::StampedTransform(tf, ros::Time::now(), "tf2", "tf1"));
-
-  obvious::Matrix matSum(3, 3);
-  matSum.setIdentity();
-  matSum = matDiff * mat1;
+  //  mat1.setIdentity();
+//  mat1(1, 2) = 1.0;
+//  mat2.setIdentity();
+//  mat2(1, 2) = 2.0;
+//  tf::Transform tf;
+//  tf.setRotation(tf::createQuaternionFromYaw(0.8));
+//  tf.setOrigin(tf::Vector3(1.0, 0.0, 0.0));
+//  mat1 = utils.tfToObviouslyMatrix3x3(tf);
+//  bc.sendTransform(tf::StampedTransform(tf, ros::Time::now(), "map", "tf1"));
+//  obvious::Matrix point(1, 3);
+//  point.setIdentity();
+//  point(0, 0) = 1.0;
+//  point = mat1 * point;
+//  geometry_msgs::PoseStamped
+//
+//  tf.setRotation(tf::createQuaternionFromYaw(1.2));
+//  tf.setOrigin(tf::Vector3(2.0, 0.0, 0.0));
+//  mat2 = utils.tfToObviouslyMatrix3x3(tf);
+//  bc.sendTransform(tf::StampedTransform(tf, ros::Time::now(), "map", "tf2"));
+//
+//
+//  obvious::Matrix matDiff(3, 3);
+//  matDiff.setIdentity();
+//  matDiff = mat2 * mat1.getInverse();
+//  matDiff.print();
+//  tf = utils.obviouslyMatrix3x3ToTf(matDiff);
+//  //bc.sendTransform(tf::StampedTransform(tf, ros::Time::now(), "tf2", "tf1"));
+//
+//  obvious::Matrix matSum(3, 3);
+//  matSum.setIdentity();
+//  matSum = matDiff * mat1;
   // bc.sendTransform(tf::StampedTransform(tf, ros::Time::now(), "map", "tf2"));
 }
 
